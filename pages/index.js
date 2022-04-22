c0d6bfe9d15e5979bb36d37d8e5ea25a408425f2
Marceau Malégeant (Soetch) <soetchdev@gmail.com>
2022-04-22 18:25:11 +0200

    Fixed a link

--- pages/index.js
+++ pages/index.js
@@ -70,15 +70,15 @@ export default function Home() {
         </div>
         <br/>
         <div className="w-full max-w-md mx-auto xl:max-w-5xl lg:max-w-5xl md:max-w-2xl md:flex bg-white rounded-lg border border-gray-200 shadow-md dark:bg-black dark:border-gray-700">
-          <a href="https://github.com/Hanabi-Corporation/HanabiOres-1.16.5">
+          <a href="https://github.com/Soetch/soetch.github.io">
             <img className="object-cover" src="/webdev-bg.png" alt="Soetch Logo"></img>
           </a>
           <div className="p-5">
-            <a href="https://github.com/Hanabi-Corporation/HanabiOres-1.16.5">
+            <a href="https://github.com/Soetch/soetch.github.io">
               < h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Soetch's Home (and portfolio)</h5>
             </a>
             <p className="mb-3 font-normal dark:text-gray-400">The code for the site you're exploring !</p>
-            <a href="https://github.com/Hanabi-Corporation/HanabiOres-1.16.5" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <a href="https://github.com/Soetch/soetch.github.io" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               🚀 See on Github
               <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </a>
